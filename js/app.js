5756004a75f18d8d2b4513989a0dfa2958fab66e
Jayson Falkner <jfalkner@pacificbiosciences.com>
2016-11-01 13:10:35 -0400

    Updated JS license notices

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
-// global config
+/**
+ * Copyright 2016, Jayson Falkner - jfalkner@gmail.com
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+// global state
 Window.App = {
     blankQuery: true,
     query: null
